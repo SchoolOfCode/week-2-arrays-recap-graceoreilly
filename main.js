fb39e8cbf5d4024973d8db0411e92a58d4527230
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-03 20:59:20 +0000

    Complete Task 2

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Task 1
+
 const favouriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"];
 
 const profitPyramidScheme = [1, 2, 4, 8, 16, 32,];
@@ -14,3 +16,26 @@ let friends = [
     {name: "Joseph", skill: "positive"},
     {name: "Max", skill: "papier-mâché"}
 ];
+
+// Task 2
+
+let options = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  console.log (options[2])
+
+  console.log (options[9])
+
+  console.log (options[4])
