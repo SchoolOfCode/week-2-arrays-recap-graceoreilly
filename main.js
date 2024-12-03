2c87d4ade529a26b162bddb3382a4be6c6d912f8
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-03 23:12:24 +0000

    Complete Task 3

--- main.js
+++ main.js
@@ -39,3 +39,13 @@ let options = [
   console.log (options[9])
 
   console.log (options[4])
+
+  // Task 3
+  let englishNumbers = ["one", "two", "three", "four", "five"];
+
+  let index = 0;
+
+  while (index < englishNumbers.length){ //every time the loop is run, it checks if index => englishNumbers.length
+    console.log (englishNumbers[index]);
+    index++; // it should print 1-5 (this is the length of englishNumbers). Once the index is less than 5, the loop will stop as the condition is false and englishNumbers[5] does not exist and index < 5 becomes false.
+  }
