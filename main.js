dc5ee799b451b40be038b5b80ef27652d36d0b06
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-03 20:55:50 +0000

    Complete Task 1

---
+++ main.js
@@ -0,0 +1,16 @@
+const favouriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"];
+
+const profitPyramidScheme = [1, 2, 4, 8, 16, 32,];
+
+let friends = [
+    {name: "Chris", skill: "changing lives"},
+    {name: "Lizzie", skill: "being friendly"},
+    {name: "Ben", skill: "code rapping"},
+    {name: "Tao", skill: "hand raising"},
+    {name: "Liz", skill: "crochet"},
+    {name: "Patrick", skill: "Call of Duty"},
+    {name: "Tim", skill: "analogies"},
+    {name: "James", skill: "friendship"},
+    {name: "Joseph", skill: "positive"},
+    {name: "Max", skill: "papier-mâché"}
+];
